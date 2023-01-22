0256e95f46142c237de0bf3def7b01afe6d218fd
scarecrow <trender.man21@gmail.com>
2023-01-23 00:00:44 +0200

    Updates

---
+++ static/js/133.4d1cc7de.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_2hw=self.webpackChunk_2hw||[]).push([[133],{9625:function(n,t,e){var c=e(8683),o=e(7496),u=e(9434),r=e(7689),s=e(184);t.Z=function(n,t){return function(e){return(0,u.v9)(o.PR)?(0,s.jsx)(n,(0,c.Z)({},e)):(0,s.jsx)(r.Fg,{to:t})}}},2133:function(n,t,e){e.r(t);var c=e(7496),o=e(9625),u=e(9434),r=e(184);t.default=(0,o.Z)((function(){return(0,u.v9)(c.PR)?(0,r.jsx)("h1",{children:"Welcome to HomePage "}):(0,r.jsx)("h1",{children:"Log In to get access to your contacts"})}),"/login")}}]);
+//# sourceMappingURL=133.4d1cc7de.chunk.js.map
